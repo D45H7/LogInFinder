30954476d6498d15b4c9f7e521b8bc6fda1f7296
_/|Arya-Kun <49186597+MrC3nd0l@users.noreply.github.com>
2020-12-06 18:39:49 +0700

    Add files via upload

---
+++ logfind.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.9
+#
+# Coded by EtcAug10
+# Forked from https://tools.d4rk5idehacker.or.id
+# Easy Code for learn
+
+import requests,random
+
+banner = """
+#  ]     ]   ] ]     ]  ]  ]
+# ]  ]  ]   ]   ]   ]     ]
+#  ]  Login Page Finder ]  ]
+# ]  by EtcAug10  ]   ]    ]
+#   ]    ]   ] ]]    ]  ] ]
+#  ]]  ]  ] ]    ]     ]   ]
+"""
+
+print(banner)
+print("\n(!) Bruteforcing to catch Login page")
+target = input("Target domain: ")
+url = "https://tools.d4rk5idehacker.or.id/Penetration-Testing/starting.php?str="+target
+hasil = requests.get(url).text
+print("\nMake result..")
+w = "abcdefghijklmnopqrstuvwxyz"
+open("hasil.html", "a").write(hasil)
+print("\nFile saved in hasil.html")
+quit("Busy here.. huh...")
